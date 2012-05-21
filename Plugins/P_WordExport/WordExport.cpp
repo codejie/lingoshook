9c9a8f19c6ee5ce675eacd138d01d9eccd321c77
codejie@gmail.com <codejie@gmail.com@911c9502-749a-b9f9-c2c5-4ebe02bd8b01>
2012-05-21 10:04:51 +0000

    LingosHook - New WordExport Plugin coding...

--- Plugins/P_WordExport/WordExport.cpp
+++ Plugins/P_WordExport/WordExport.cpp
@@ -2,6 +2,7 @@
 #include "Consts.h"
 
 #include "WordExportDialog.h"
+#include "WordExportV2Dialog.h"
 
 #include "WordExport.h"
 
@@ -22,9 +23,9 @@ int WordExport::LoadProperty(PropertyData& data) const
 {
     data.m_strLabel = wxT("WordExport");
     data.m_strName = wxT("Word Export");
-    data.m_strVersion = wxT("0.0.1");
+    data.m_strVersion = wxT("1.0.0");
     data.m_strAuthor = wxT("codejie");
-    data.m_strDescription = wxT("Export words from LingosHook to a Text file.");
+    data.m_strDescription = wxT("Export words from LingosHook to a HTML/Text file.");
 //    data.m_strDetail = wxT("NONE.");
 
     return 0;
@@ -34,8 +35,8 @@ int WordExport::Active(wxApp* papp, wxWindow* pwindow)
 {
     //wxMessageBox(_objDBAccess->Database().GetVersion());
 
-    WordExportDialog dlg(_objDBAccess, pwindow, wxID_ANY, wxEmptyString);
-	//WordExportV2Dialog dlg(_objDBAccess, pwindow, wxID_ANY, wxEmptyString);
+    //WordExportDialog dlg(_objDBAccess, pwindow, wxID_ANY, wxEmptyString);
+	WordExportV2Dialog dlg(_objDBAccess, pwindow, wxID_ANY, wxEmptyString);
     dlg.ShowModal();
 
     return 0;
